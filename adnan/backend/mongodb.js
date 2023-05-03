acaf8cbc08f77989e2e522c6d2157f986c04ee43
AdnanKhan2022Skipq <adnan.khan.skipq@gmail.com>
2023-05-03 15:49:52 +0500

    week 2/3 capstone project

--- adnan/backend/mongodb.js
+++ adnan/backend/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-const uri = "mongodb+srv://adnankhanskipq:adnankskipq@cluster0.gkf26ri.mongodb.net/?retryWrites=true&w=majority";
+
 
 const coonectToMongo=()=>{
     mongoose.connect(uri,console.log('connected'))
